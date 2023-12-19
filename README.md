894d656b1594fe4beac0c59d27608931a50d6e49
kanak.sachin.dahake <kanak.sachin.dahake@pwc.com>
2023-12-19 14:56:12 +0530

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# 100-days-of-AI-Writing
+# 100-days-of-AI-Writin
+
+This initiative explores the latest trends and advancements in cutting-edge technologies, focusing on industries that are poised to become the next trillion-dollar markets.
+
+## Topics:
+- Public blockchain, 
+- AI 
+- Robotics 
+- Energy Storage 
+- Multi Omics Sequencing
+
+---
+
+You'll find an overview of the key topics covered. For each topic you can find the link of article to further explore.
+
+
+<!-- TABLE START -->
+
+| Sr.No | Category | Name | Description | Tags | Link |
+|-------|----------|------|-------------|------|------|
+| 1 | AI | AI in Healthcare: Predictive Analytics | How AI is revolutionizing healthcare by predicting diseases and patient outcomes. | AI, Healthcare, Predictive Analytics, Innovation | [AI-in-Healthcare:-Predictive-Analytics.md](https://github.com/Kanakjr/100-days-of-AI-Writing/blob/main/articles/AI-in-Healthcare:-Predictive-Analytics.md) |
+| 2 | Public Blockchain | Blockchain for Voting Systems | Exploring the potential of blockchain technology to create secure and transparent voting systems. | Blockchain, Voting, Security, Transparency | [Blockchain-for-Voting-Systems.md](https://github.com/Kanakjr/100-days-of-AI-Writing/blob/main/articles/Blockchain-for-Voting-Systems.md) |
+| 3 | Robotics | Autonomous Robots in Manufacturing | The rise of autonomous robots and their impact on efficiency and safety in manufacturing. | Robotics, Manufacturing, Autonomous, Efficiency | [Autonomous-Robots-in-Manufacturing.md](https://github.com/Kanakjr/100-days-of-AI-Writing/blob/main/articles/Autonomous-Robots-in-Manufacturing.md) |
+| 4 | Energy Storage | Advancements in Battery Technologies | Innovations in energy storage that are enabling the transition to renewable energy sources. | Energy Storage, Batteries, Renewable Energy, Innovation | [Advancements-in-Battery-Technologies.md](https://github.com/Kanakjr/100-days-of-AI-Writing/blob/main/articles/Advancements-in-Battery-Technologies.md) |
+| 5 | Multi Omics Sequencing | Genomics and Personalized Medicine | How multi omics sequencing is paving the way for personalized medical treatments. | Genomics, Personalized Medicine, Multi Omics, Healthcare | [Genomics-and-Personalized-Medicine.md](https://github.com/Kanakjr/100-days-of-AI-Writing/blob/main/articles/Genomics-and-Personalized-Medicine.md) |
+<!-- TABLE END -->
+
+
+
+
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from datetime import datetime
 from article import generate_article
 from utils import get_llm
 from dotenv import load_dotenv
+import os
 
 load_dotenv('./.env')
 
@@ -24,6 +25,44 @@ def git_add_and_push(topic):
     subprocess.run(["git", "add", "."])
     subprocess.run(["git", "commit", "-m", f"Added {topic} article"])
     subprocess.run(["git", "push"])
+
+def update_readme_from_csv(readme_file, csv_file):
+    # Read data from CSV
+    with open(csv_file, 'r', newline='', encoding='utf-8') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        rows_with_links = [row for row in csv_reader if row.get('Link')]
+
+    # Generate table markdown
+    GIT_ARTICLE_BASEURL = os.environ.get("GIT_ARTICLE_BASEURL")
+    table_markdown = "| Sr.No | Category | Name | Description | Tags | Link |\n"
+    table_markdown += "|-------|----------|------|-------------|------|------|\n"
+    for row in rows_with_links:
+        table_markdown += f"| {row['Sr.No']} | {row['Category']} | {row['Name']} | {row['Description']} | {row['Tags']} | [{row['Link']}]({GIT_ARTICLE_BASEURL}{row['Link']}) |\n"
+
+    # Read existing README content
+    with open(readme_file, 'r', encoding='utf-8') as readme:
+        readme_content = readme.read()
+
+    # Find the position of the existing table (if it exists)
+    table_start = readme_content.find("<!-- TABLE START -->")
+    table_end = readme_content.find("<!-- TABLE END -->")
+
+    # Update README content with the new table
+    if table_start != -1 and table_end != -1:
+        updated_readme_content = (
+            readme_content[:table_start] +
+            "<!-- TABLE START -->\n\n" +
+            table_markdown +
+            "<!-- TABLE END -->\n\n" +
+            readme_content[table_end + len("<!-- TABLE END -->"):]
+        )
+    else:
+        # If the table doesn't exist, simply append it to the end of the README
+        updated_readme_content = readme_content + "\n\n<!-- TABLE START -->\n\n" + table_markdown + "<!-- TABLE END -->\n"
+
+    # Write the updated content back to README
+    with open(readme_file, 'w', encoding='utf-8') as readme:
+        readme.write(updated_readme_content)
 
 
 def main():
@@ -48,6 +87,9 @@ def main():
 
     # Update the CSV with the .md file location
     update_csv(csv_filename, int(selected_topic["Sr.No"]), md_filename)
+
+    # Update readme Table
+    update_readme_from_csv('README.md', csv_file=csv_filename)
 
     # Git add and push
     git_add_and_push(selected_topic["Name"])
